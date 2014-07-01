d2909a4a36eb7fa4dc416bdae5dc0f30bac44e05
Heri Parid Pardian <h.pardian@samsung.com>
2014-07-01 16:37:58 +0900

    add unittest script

--- manage.py
+++ manage.py
@@ -1,6 +1,19 @@
-from app import create_app
+#!/usr/bin/env python
+import os
 
-app = create_app('default')
+from app import create_app, db
+from flask_script import Manager
+from flask_migrate import Migrate
+
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+manager = Manager(app)
+migrate = Migrate(app, db)
+
+@manager.command
+def test():
+    import unittest
+    test = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(test)
 
 if __name__ == '__main__':
-    app.run()
+    manager.run()
